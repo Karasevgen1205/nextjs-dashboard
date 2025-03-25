3e57b198533659aca23d6a2c027e2ed4f5afb7d9
“Karasevgen1205” <karasevgen1205@yandex.ru>
2025-03-25 22:27:04 +0300

    add CRUD for invoices

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,7 @@
 import { GlobeAltIcon } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
-import styles from "./home.module.css";
 
-interface IAcmeLogoProps {
-  isSkipAnimate?: boolean;
-}
-
-export default function AcmeLogo({ isSkipAnimate }: IAcmeLogoProps) {
-  // ${isSkipAnimate ? styles.typewriter : ""}
+export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white p-2`}
