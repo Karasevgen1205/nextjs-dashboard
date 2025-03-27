3b6d806237217f150a5beba29b42a4d53492d8b6
“Karasevgen1205” <karasevgen1205@yandex.ru>
2025-03-27 23:31:37 +0300

    add auth and meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import { inter } from "@/app/ui/fonts";
 import "@/app/ui/global.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Evgeniy Karas",
+    default: "Evgeniy Karas",
+  },
+  description: "The official page of front-end developer - Evgeniy Karas",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import Image from "next/image";
 import { lusitana } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Main page",
+};
 
 export default function Page() {
   return (
